21ea78a0c165883b7377b79f25de75696e26542c
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-10 14:54:28 -0800

    add CharacterDetail

---
+++ src/components/details/CharacterDetail.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CharacterDetail = ({ image, name, status, species }) => (
+  <figure>
+    <img src={image} alt={name} />
+    <figcaption>
+      <p>{name}</p>
+      <p>{status}</p>
+      <p>{species}</p>
+    </figcaption>
+  </figure>
+);
+
+CharacterDetail.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  species: PropTypes.string.isRequired,
+};
+
+export default CharacterDetail;
