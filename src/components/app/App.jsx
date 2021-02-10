a5f64e10d81c499613ee0618fa3020507c294c4e
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-10 14:02:32 -0800

    add file structure and Toast

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from '../home/Home';
+import CharactersPage from '../characters/CharactersPage';
 import Header from '../header/Header';
 
 export default class App extends Component {
@@ -10,7 +10,7 @@ export default class App extends Component {
         <Router>
           <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={AllC} />
           </Switch>
         </Router>
       </div>
