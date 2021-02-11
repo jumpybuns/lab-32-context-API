f00dbeda657a8918a171dd409b22849551030cba
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-10 16:54:14 -0800

    amidst the theme toggle

--- src/hooks/Theme.jsx
+++ src/hooks/Theme.jsx
@@ -7,11 +7,4 @@ export const useTheme = () => {
   const themeToggler = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light');
   };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVideos(dummyData);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
 };
